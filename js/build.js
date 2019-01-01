58dce4b5c0ff49ec17d9d77bd1c28393dec33b23
Alan Bickel <alan.bickel@gmail.com>
2019-01-01 12:21:09 -0500

    initial DOM construction

--- js/build.js
+++ js/build.js
@@ -56,7 +56,7 @@ var Calendar = (function () {
         /**
          * step forward | back a month
          */
-        this.getSiblingMonth = function (movement) {
+        this.getSibling = function (movement) {
             var response = {
                 month: _this.currentMonth,
                 year: _this.currentYear
@@ -87,11 +87,85 @@ var Calendar = (function () {
          * build calendar table
          */
         this.render = function () {
+            _this.parent.innerHTML = "";
+            var tbl_html = document.createDocumentFragment();
+            var html = document.createDocumentFragment();
+            var feb_num_days = "";
+            var counter = 1;
+            var dateNow = null;
+            var daysInThisMonth = _this.daysInMonth(_this.currentYear, _this.currentMonth);
+            var prev = _this.getSibling('prev');
+            var next = _this.getSibling('next');
+            var daysInPreviousMonth = _this.daysInMonth(prev.year, prev.month);
+            var daysInNextMonth = _this.daysInMonth(next.year, next.month);
+            var weekdays = _this.currentDate.getDay();
+            var weekdays2 = weekdays;
+            //begin DOM construction
+            var tr = document.createElement("tr");
+            tbl_html.appendChild(tr);
+            //add trailing days of last month
+            while (weekdays > 0) {
+                var trailingDayNumber = (daysInPreviousMonth - (weekdays - 1));
+                var td = document.createElement('td');
+                td.innerText = trailingDayNumber.toString();
+                td.classList.add("monthPre");
+                td.classList.add("dayPast");
+                td.classList.add("day");
+                tr.appendChild(td);
+                weekdays--;
+            }
+            //build calendar body
+            while (counter <= daysInThisMonth) {
+                //start new line.
+                if (weekdays2 > 6) {
+                    weekdays2 = 0;
+                    tr = document.createElement("tr");
+                    tbl_html.appendChild(tr);
+                }
+                //create element for each day
+                var td = document.createElement('td');
+                td.classList.add("day");
+                td.classList.add("day-current-month");
+                //in the past?
+                var yearInPast = _this.currentYear < _this.baseYear;
+                var yearInPresent = _this.currentYear == _this.baseYear;
+                var yearInFuture = _this.currentYear > _this.baseYear;
+                var monthInPast = (yearInPast || yearInPresent) && _this.currentMonth < _this.baseMonth;
+                var monthInPresent = yearInPresent && _this.currentMonth == _this.baseMonth;
+                var monthInFuture = (yearInFuture || yearInPresent && _this.currentMonth > _this.baseMonth);
+                if (yearInPast || monthInPast || (monthInPresent && _this.currentDate.getDate() < _this.baseDate.getDate()))
+                    td.classList.add("dayPast");
+                if (monthInPresent && (_this.currentDate.getDate() == _this.baseDate.getDate()))
+                    td.classList.add("dayNow");
+                if (monthInFuture || monthInPresent && _this.currentDate.getDate() > _this.baseDate.getDate())
+                    td.classList.add("dayFuture");
+                //persist date value
+                td.dataset.date = _this.dateToString(_this.currentYear, _this.currentMonth, _this.currentDate.getDate());
+            }
+        };
+        this.dateToString = function (y, m, d) {
+            var yy = y.toString(), mm = (m + 1).toString(), dd = d.toString();
+            while (mm.length < 2)
+                mm = "0" + mm;
+            while (dd.length < 2)
+                dd = "0" + dd;
+            return yy + "-" + mm + "-" + dd;
+        };
+        this.daysInMonth = function (year, month) {
+            if (month === 1)
+                return _this.febLength(year);
+            else
+                return parseInt(daysPerMonth[month]);
+        };
+        this.febLength = function (year) {
+            if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0)
+                return 29;
+            else
+                return 28;
         };
         this.parent = parentElement;
         //set default date
         this.baseDate = dateOverride ? dateOverride : new Date();
-        debugger;
         this.currentDate = this.baseDate;
         //month
         this.baseMonth = this.baseDate.getMonth();
--- ts/calendar.ts
+++ ts/calendar.ts
@@ -19,7 +19,6 @@ class Calendar {
     this.parent = parentElement;
     //set default date
     this.baseDate = dateOverride ? dateOverride : new Date();
-    debugger;
     this.currentDate = this.baseDate;
     //month
     this.baseMonth = this.baseDate.getMonth();
@@ -39,7 +38,7 @@ class Calendar {
   /**
    * step forward | back a month
    */
-  getSiblingMonth = (movement : string): any => {
+  getSibling = (movement : string): any => {
 
     let response = {
       month : this.currentMonth, 
@@ -72,6 +71,89 @@ class Calendar {
    * build calendar table
    */
   render = (): void => {
+    this.parent.innerHTML = "";
 
+    let tbl_html = document.createDocumentFragment();
+    let html = document.createDocumentFragment();
+    let feb_num_days = "";
+    let counter = 1;
+    let dateNow = null;
+
+    let daysInThisMonth = this.daysInMonth(this.currentYear, this.currentMonth);
+    let prev = this.getSibling('prev');
+    let next = this.getSibling('next');
+
+    let daysInPreviousMonth = this.daysInMonth(prev.year, prev.month);
+    let daysInNextMonth = this.daysInMonth(next.year, next.month);
+
+    let weekdays = this.currentDate.getDay();
+    let weekdays2 = weekdays;
+
+
+    //begin DOM construction
+    let tr = document.createElement("tr");
+    tbl_html.appendChild(tr);
+    //add trailing days of last month
+    while(weekdays > 0){
+      let trailingDayNumber = (daysInPreviousMonth - (weekdays -1));
+      let td = document.createElement('td');
+      td.innerText = trailingDayNumber.toString();
+      td.classList.add("monthPre");
+      td.classList.add("dayPast");
+      td.classList.add("day");
+      tr.appendChild(td);
+      weekdays--;
+    }
+    //build calendar body
+    while (counter <= daysInThisMonth){
+      //start new line.
+      if (weekdays2 > 6) {
+        weekdays2 = 0;
+        tr = document.createElement("tr");
+        tbl_html.appendChild(tr);
+      }
+      //create element for each day
+      let td = <HTMLElement>document.createElement('td');
+      td.classList.add("day");
+      td.classList.add("day-current-month");
+      //in the past?
+      let yearInPast = this.currentYear < this.baseYear;
+      let yearInPresent = this.currentYear == this.baseYear;
+      let yearInFuture = this.currentYear > this.baseYear;
+      let monthInPast = (yearInPast || yearInPresent) && this.currentMonth < this.baseMonth;
+      let monthInPresent = yearInPresent && this.currentMonth == this.baseMonth;
+      let monthInFuture = (yearInFuture || yearInPresent && this.currentMonth >  this.baseMonth);
+
+      if(yearInPast || monthInPast || (monthInPresent && this.currentDate.getDate() < this.baseDate.getDate()))  
+        td.classList.add("dayPast");
+      if(monthInPresent && (this.currentDate.getDate() == this.baseDate.getDate()) )
+        td.classList.add("dayNow");
+      if(monthInFuture || monthInPresent && this.currentDate.getDate() > this.baseDate.getDate())
+      td.classList.add("dayFuture");
+      //persist date value
+      (<any>td.dataset).date = this.dateToString(this.currentYear, this.currentMonth, this.currentDate.getDate());
+      // add conflicts | entries from data file
+
+
+    }
+  }
+
+  dateToString = (y:number, m: number, d: number): string => {
+    var yy = y.toString(),
+        mm = (m + 1).toString(),
+        dd = d.toString();
+      while (mm.length < 2) mm = "0" + mm;
+      while (dd.length < 2) dd = "0" + dd;
+      return yy + "-" + mm + "-" + dd;
+  }
+
+  daysInMonth = (year: number, month : number): number=> {
+    if (month === 1) return this.febLength(year);
+      else return parseInt(daysPerMonth[month]);
+  }
+
+  febLength = (year : number): number => {
+    if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0) return 29;
+    else return 28;
   }
 }
