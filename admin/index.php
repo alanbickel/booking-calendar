225d2cc3007fbbf64f86c2e0bb585050ba753b5a
Alan Bickel <alan.bickel@gmail.com>
2019-01-06 16:37:48 -0500

    implement admin updates

--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,7 @@
 
 <head>
 
-<link rel="stylesheet"  href="./vendor/font-awesome-4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="./css/styles.css">
+<link rel="stylesheet" href="../vendor/font-awesome-4.7.0/css/font-awesome.css">
 <script
  src="https://code.jquery.com/jquery-3.3.1.min.js"
  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -24,11 +23,22 @@
       dataType : "JSON"
     })
     .done((formData) => {
-
-      calendar = new Calendar(document.getElementById('calendar-div'), formData);
+      //initialize calendar, provide form template
+      calendar = new Calendar(document.getElementById('calendar-div') );
+      //build user input form
+      calendar.createModal("../vendor/js/malsup/blockui.js",formData );
+      //style for modal form
+      calendar.loadFormStyles('../css/form-styles.css');
+      //style for calendar
+      calendar.loadSelfStyles("../css/styles.css");
+      //data source
       calendar.setDriverLocation("../services/dates.dat");
+      //where to send updates
       calendar.setEndpoint("../services/update/");
-      //get date information & render 
+      //customise form response messages
+      calendar.setRequestCompleteMessage('success', "Date status has been updated.");
+      calendar.setRequestCompleteMessage('failure', "Unable to update date status.");
+      //get data, render calendar
       calendar.getData();
     })
    });
